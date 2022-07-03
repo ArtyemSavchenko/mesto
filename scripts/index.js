713cbdc7326e1e5a6b08cd95254c388dc6db4e77
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-07-03 20:11:09 +0300

    style

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,24 @@
-const profilePopupEl = document.querySelector('.popup');
-const openProfileBtnEl = document.querySelector('.profile__edit-btn');
-const closeProfileBtnEl = document.querySelector('.popup__close-btn');
-const submitProfileBtnEl = document.querySelector('.popup__submit-btn');
+const profilePopup = document.querySelector('.popup');
+const openProfileBtn = document.querySelector('.profile__edit-btn');
+const closeProfileBtn = document.querySelector('.popup__close-btn');
+const submitProfileBtn = document.querySelector('.popup__submit-btn');
 
-const inputNameEl = document.querySelector('.popup__input_type_name');
-const inputAboutEl = document.querySelector('.popup__input_type_about');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputAbout = document.querySelector('.popup__input_type_about');
 
-openProfileBtnEl.addEventListener('click', () => {
-  inputNameEl.value = document.querySelector('.profile__name').textContent;
-  inputAboutEl.value = document.querySelector('.profile__about').textContent;
-  profilePopupEl.classList.toggle('popup_active');
+openProfileBtn.addEventListener('click', () => {
+  inputName.value = document.querySelector('.profile__name').textContent;
+  inputAbout.value = document.querySelector('.profile__about').textContent;
+  profilePopup.classList.toggle('popup_active');
 });
 
-closeProfileBtnEl.addEventListener('click', () => {
-  profilePopupEl.classList.toggle('popup_active');
+closeProfileBtn.addEventListener('click', () => {
+  profilePopup.classList.toggle('popup_active');
 });
 
-submitProfileBtnEl.addEventListener('click', (evt) => {
+submitProfileBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
-  document.querySelector('.profile__name').textContent = inputNameEl.value;
-  document.querySelector('.profile__about').textContent = inputAboutEl.value;
-  profilePopupEl.classList.toggle('popup_active');
+  document.querySelector('.profile__name').textContent = inputName.value;
+  document.querySelector('.profile__about').textContent = inputAbout.value;
+  profilePopup.classList.toggle('popup_active');
 });
-
