c192afb20b1a9077b554a876329e98ca9025d6c7
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-07-04 20:04:21 +0300

    fix замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 const profilePopup = document.querySelector('.popup');
-const popupWindow = document.querySelector('.popup__window'); // для закрытия попапа при клике по пустой области
 const openProfileBtn = document.querySelector('.profile__edit-btn');
 const closePopupBtn = document.querySelector('.popup__close-btn');
 const userForm = document.querySelector('.popup__user-form');
@@ -8,7 +7,6 @@ const inputName = document.querySelector('.popup__input_type_name');
 const profileName = document.querySelector('.profile__name');
 const inputAbout = document.querySelector('.popup__input_type_about');
 const profileAbout = document.querySelector('.profile__about');
-
 
 function openClosePopup (popup) {
   popup.classList.toggle('popup_active');
@@ -23,12 +21,6 @@ openProfileBtn.addEventListener('click', () => {
 closePopupBtn.addEventListener('click', () => {
   openClosePopup(profilePopup);
 });
-profilePopup.addEventListener('click', () => {
-  openClosePopup(profilePopup);
-});
-popupWindow.addEventListener('click', (evt) => {
-  evt.stopPropagation()
-});
 
 userForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
@@ -36,15 +28,3 @@ userForm.addEventListener('submit', (evt) => {
   profileAbout.textContent = inputAbout.value;
   openClosePopup(profilePopup);
 });
-
-//like mechanic
-const likeBtns = document.querySelectorAll('.card__like');
-
-function toggleLike () {
-  this.classList.toggle('card__like_active');
-}
-
-likeBtns.forEach(btn => {
-  btn.addEventListener('click', toggleLike);
-});
-//---
