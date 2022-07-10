77dc077379ae19dada840e964a59db6e3a03645d
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-07-10 07:56:17 +0300

    fix: Закрытие попапа по пустой области без костылей. Перенес обработчики клика, лайка и открытия картинки на карточки.

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,19 @@ const popupImgPic = popupImg.querySelector('.card-image__img');
 const popupImgCaption = popupImg.querySelector('.card-image__caption');
 const sectionCards = document.querySelector('.cards');
 const cardTemplate = document.querySelector('#card').content;
+const popups = [
+  popupProfile,
+  popupAddCard,
+  popupImg
+]
+
+popups.forEach(popup => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target === evt.currentTarget || evt.target.classList.contains('popup__close-btn'))
+      closePopup(popup);
+  });
+});
+//evt.target === evt.currentTarget - 🔥сам бы не додумался до такого изящного решения, спасибо
 
 function createCard(title, src) {
   const card = cardTemplate.querySelector('.card').cloneNode(true);
@@ -20,8 +33,12 @@ function createCard(title, src) {
   const cardImg = card.querySelector('.card__img');
   cardImg.src = src;
   cardImg.alt = title;
+  cardImg.addEventListener('click', () => openPopupImg(card));
+  card.querySelector('.card__like').addEventListener('click', () => likeCard(card));
+  card.querySelector('.card__del').addEventListener('click', () => delCard(card));
   return card;
 }
+
 function addNewCard(card) {
   sectionCards.prepend(card);
 }
@@ -41,44 +58,26 @@ btnOpenProfile.addEventListener('click', () => {
   popupProfileAbout.value = profileAbout.textContent;
   openPopup(popupProfile);
 });
-popupProfile.querySelector('.popup__close-btn').addEventListener('click', evt => {
-  evt.stopPropagation();
-  closePopup(popupProfile);
-});
+
 popupProfile.addEventListener('submit', evt => {
   evt.preventDefault();
   profileName.textContent = popupProfileName.value;
   profileAbout.textContent = popupProfileAbout.value;
   closePopup(popupProfile);
 });
-popupProfile.addEventListener('click', () => closePopup(popupProfile));
-popupProfile.querySelector('.popup__window').addEventListener('click', evt => evt.stopPropagation());
 
 btnAddCard.addEventListener('click', () => {
   popupCardName.value = '';
   popupCardUrl.value = '';
   openPopup(popupAddCard);
 });
-popupAddCard.querySelector('.popup__close-btn').addEventListener('click', evt => {
-  evt.stopPropagation();
-  closePopup(popupAddCard);
-});
+
 popupAddCard.addEventListener('submit', evt => {
   evt.preventDefault();
   const card = createCard(popupCardName.value, popupCardUrl.value);
   addNewCard(card);
   closePopup(popupAddCard);
 });
-popupAddCard.addEventListener('click', () => closePopup(popupAddCard));
-popupAddCard.querySelector('.popup__window').addEventListener('click', evt => evt.stopPropagation());
-
-popupImg.querySelector('.popup__close-btn').addEventListener('click', evt => {
-  evt.stopPropagation();
-  closePopup(popupImg);
-});
-popupImg.addEventListener('click', () => closePopup(popupImg));
-popupImg.querySelector('.card-image__img').addEventListener('click', evt => evt.stopPropagation());
-popupImg.querySelector('.card-image__caption').addEventListener('click', evt => evt.stopPropagation());
 
 function likeCard (card) {
   card.querySelector('.card__like').classList.toggle('card__like_active');
@@ -92,19 +91,5 @@ function openPopupImg (card) {
   popupImgCaption.textContent = card.querySelector('.card__title').textContent;
   openPopup(popupImg);
 }
-sectionCards.addEventListener('click', evt => {
-  if (evt.target.classList.contains('card__like')) {
-    likeCard (evt.target.closest('.card'));
-    return;
-  }
-  if (evt.target.classList.contains('card__del')) {
-    delCard (evt.target.closest('.card'));
-    return;
-  }
-  if (evt.target.classList.contains('card__img')) {
-    openPopupImg(evt.target.closest('.card'));
-    return;
-  }
-});
 
 samplesCards.forEach(el => addNewCard(createCard(el.title, el.src)));
